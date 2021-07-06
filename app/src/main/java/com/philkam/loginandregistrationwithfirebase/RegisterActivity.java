07a109a82f94d0295fb503bd458c9016e039b615
Philip Marfo <philcho98@gmail.com>
2021-07-06 03:14:38 +0000

    Added dependencies to the gradle file to help with firefox authentication.

--- app/src/main/java/com/philkam/loginandregistrationwithfirebase/RegisterActivity.java
+++ app/src/main/java/com/philkam/loginandregistrationwithfirebase/RegisterActivity.java
@@ -3,12 +3,24 @@ package com.philkam.loginandregistrationwithfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText EditTextEmail;
+    private EditText EditTextPassword;
+    private Button BtnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditTextEmail = findViewById(R.id.editTextEmailAddress);
+        EditTextPassword = findViewById(R.id.editTextPassword);
+        BtnRegister = findViewById(R.id.register);
+
+
     }
 }
